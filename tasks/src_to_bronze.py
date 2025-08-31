451082cb587f02a5c4c2a635fe3a3817e71199d6
Yu-Shan Wei <yuweiy@student.ethz.ch>
2025-08-31 16:41:50 +0200

    correct the path name back

--- tasks/src_to_bronze.py
+++ tasks/src_to_bronze.py
@@ -1,7 +1,7 @@
 from src.stream_loader import ingest_stream
 
 ingest_stream(
-    input_path="/dbfs/Volumes/dev/default/events_stream",
+    input_path="dbfs:/Volumes/dev/default/events_stream",
     table_name="dev.default.view_events",
     checkpoint_path="dbfs:/Volumes/dev/default/autoloader_checkpoints/view_events"
 )
