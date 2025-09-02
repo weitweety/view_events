958a8df8742a0abd49f1313fbb495c3e48b1501f
Yu-Shan Wei <yuweiy@student.ethz.ch>
2025-09-02 15:22:42 +0200

    add analyser

---
+++ src/analyser.py
@@ -0,0 +1,35 @@
+from pyspark.sql.functions import col, when
+
+def aggregate_reactions(df):
+    agg_reaction_df = (
+        df
+        .groupBy("reaction")
+        .count()
+        .withColumnRenamed("count", "reaction_count")
+        .withColumn("reaction_ratio", col("reaction_count") / total)
+    )
+    return agg_reaction_df
+
+def aggregate_duration(df):
+    bin_duration = df.withColumn("bin_duration", when(col("view_duration") <= 5, "0-5")
+                                .when((col("view_duration") > 5) & (col("view_duration") <= 10), "5-10")
+                                .when((col("view_duration") > 10) & (col("view_duration") <= 20), "10-20")
+                                .otherwise("Invalid view time"))
+    agg_bin_duration = (
+        bin_duration
+        .groupBy("bin_duration")
+        .count()
+        .withColumnRenamed("count", "bin_duration_count")
+        .withColumn("bin_duration_ratio", col("bin_duration_count") / total)
+    )
+    return agg_bin_duration
+
+def aggregate_visits(df):
+    agg_visit_df = (
+        df
+        .groupBy("visit_website")
+        .count()
+        .withColumnRenamed("count", "category_count")
+        .withColumn("category_ratio", col("category_count") / total)
+    )
+    return agg_visit_df
---
+++ tasks/bronze_to_silver.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+from src.analyser import *
+
+spark = SparkSession.builder.getOrCreate()
+
+df = spark.table("dev.default.view_events")
+total = df.count()
+
+agg_reaction_df = aggregate_reactions(df)
+agg_bin_duration = aggregate_duration(df)
+agg_visit_df = aggregate_visits(df)
+
+agg_reaction_df.write.saveAsTable("dev.default.agg_reaction_df", mode="overwrite")
+agg_bin_duration.write.saveAsTable("dev.default.agg_bin_duration", mode="overwrite")
+agg_visit_df.write.saveAsTable("dev.default.agg_visit_df", mode="overwrite")
